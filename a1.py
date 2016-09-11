1d23253d456aa5b1d596e6ad7efbdbb97f97ea0f
Viresh <virresh26@gmail.com>
2016-09-11 12:37:59 +0530

    major code revision, added negative amount error handle

--- a1.py
+++ a1.py
@@ -1,16 +1,18 @@
 ''' Module a1 for HW 1
 	This module has functions for fetching the currency data
-	designed for python 2.7
+	designed for python 3
 
 	Team Information :
 	Partner 1						Partner 2
 	Name:		Viresh Gupta		Name:		Saatvik Jain
-	Roll No:	2016116				Roll No:	2016261
+	Roll No:	2016118				Roll No:	2016261
 '''
-import urllib
+import urllib.request
 
 ''' Global Variables '''
+
 err=''
+
 ''' ---------------- '''
 
 def currency_response(from_c,to_c,amount_from):
@@ -23,20 +25,23 @@ def currency_response(from_c,to_c,amount_from):
 		amount_from		float value representing the amount to convert
 	'''
 	query_string = "http://cs1110.cs.cornell.edu/2015fa/a1server.php?from=" + from_c +"&to=" + to_c + "&amt=" + str(amount_from)
-	web_page = urllib.urlopen(query_string)		#contains the web page returns
-	string = str(web_page.read())			#contains the JSON string
+	web_page = urllib.request.urlopen(query_string)		#contains the web page returned
+	string = web_page.read().decode('utf-8')			#contains the JSON string
 	return string
 
 def has_error(json_string):
-	'''	This functions return's True/False depending on whether the
+	'''	This function return's True/False depending on whether the
 		retrieved JSON string has an error or not.
+		Also checks if the amount is non-negative as negative currencies are not used
 		Parameter:					Precondition:
-		json -	String to process	It is the responce to a currency query
+		json -	String to process	It is the response to a currency query
 	'''
-	
-	if(json_string[json_string.find("valid")+9:(json_string.find("valid")+14)]=="false"):
-		global err 
+	global err
+	if(json_string[json_string.find("valid")+9:(json_string.find("valid")+14)]=="false"): 
 		err = json_string[json_string.find("error")+10:json_string.find("}")-2]	#sets the specification of what error occurred
+		return True
+	elif(float(before_space(json_string[json_string.find("rhs")+8:])) < 0.0):
+		err = "Negative Amount given"
 		return True
 	else:
 		return False				#no error
@@ -83,12 +88,13 @@ def exchange(from_c,to_c,amount_from):
 		return converted_value
 
 if(__name__ == '__main__'):
-	fromCode = raw_input("Enter From Country Code :\t")
-	toCode = raw_input("Enter To Country Code :\t\t")
+	fromCode = input("Enter From Country Code :\t")
+	toCode = input("Enter To Country Code :\t\t")
 
 	#invalid amount error handling code
+	#negative currency amounts are blocked here at the has_error stage
 	try:
-		amt = float(raw_input("Enter the amount to convert:\t"))
+		amt = float(input("Enter the amount to convert:\t"))
 	except ValueError:
 		print("Wrong Amount Entered.\n")
 		exit(0)
--- a1test.py
+++ a1test.py
@@ -7,20 +7,26 @@ from a1 import *
 	Team Information :
 	Partner 1						Partner 2
 	Name:		Viresh Gupta		Name:		Saatvik Jain
-	Roll No:	2016116				Roll No:	2016261
+	Roll No:	2016118				Roll No:	2016261
 """
 
 class a1test(unittest.TestCase):
 	
 	def test_exchange(self):
 		"""Testing the exchange function"""
-		self.assertEqual(exchange("INVALID","INR",5),-1)
-		self.assertEqual(exchange("USD","INR",6),333.54735)
+		self.assertEqual(exchange("INVALID","LKR",5),-1)
+		self.assertEqual(exchange("USD","INR",5),333.54735)
+		self.assertEqual(exchange("LTL","ZWL",13),1377.2797844181)
+		self.assertEqual(exchange("JPY","INR",5000000),3211428.8492682)
+		self.assertEqual(exchange("GBP","AED",-5),-1)
+
 	def test_currency_response(self):
 		"""Testing the currency_response function"""
 		self.assertEqual(currency_response("USD","INR",1.0),'{ "lhs" : "1 United States Dollar", "rhs" : "66.70947 Indian Rupees", "valid" : true, "error" : "" }')
-	def test_beforeSpace(self):
-		self.assertEqual(before_space("Hello World"),"Hello")
+
+	def test_hasError(self):
+		"""Testing the has_error() function"""
+		self.assertEqual(has_error('{ "lhs" : "1 United States Dollar", "rhs" : "66.70947 Indian Rupees", "valid" : true, "error" : "" }'),False)
 
 
 if __name__=='__main__':
